0b405623a63a765ff1b270834fee13a22665360c
mahenv <mahendrauppari044@gmail.com>
2024-08-22 01:07:53 -0400

    first commit

---
+++ components/Nav.tsx
@@ -0,0 +1,41 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "services",
+    path: "/services",
+  },
+  {
+    name: "work",
+    path: "/work",
+  },
+  {
+    name: "about",
+    path: "/about",
+  },
+];
+const Nav = () => {
+    const pathName = usePathname()
+  return (
+    <nav className="flex gap-8">
+      {links.map((link, index) => {
+        return (
+          <Link key={link.path} href={link.path}
+          className={`${pathName === link.path && "text-accent border-b-2 border-accent"}
+          capitalize font-medium hover:text-accent transition-all`}>
+
+            {link.name}
+          </Link>
+        );
+      })}
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ components/Stairs.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {motion} from "framer-motion";
+
+const stairAnimation = {
+    initial:{
+        top: "0%"
+
+    },
+    animate:{
+        top:"100%"
+    },
+    exit:{
+        top:["100%","0%"]
+        
+    }
+}
+const Stairs = () => {
+  return (
+
+    <>
+    f</>
+  )
+}
+
+export default Stairs
